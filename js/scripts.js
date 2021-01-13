505c7f95f362f825d29cceb073a59051c690e169
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-13 15:42:35 -0800

    add date input
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
   $("form1").submit(function(event) {
     const beverage = parseInt($("#beverage").val());
     const flavor = $("input:radio)[name=flavor]:checked").val();
+    const dob = $("#born").val();
   });
 });
