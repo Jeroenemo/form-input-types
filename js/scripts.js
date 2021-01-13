b1cf23969d0a69c8f70b6446882791a3c540fa5e
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-13 15:40:22 -0800

    add icecream radio button
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("form1").submit(function(event) {
     const beverage = parseInt($("#beverage").val());
+    const flavor = $("input:radio)[name=flavor]:checked").val();
   });
 });
